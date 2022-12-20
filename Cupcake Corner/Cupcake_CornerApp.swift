565c19bd8249ebc4c2ad19b9c0841775a6b571e8
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-20 18:56:40 -0300

    Initial Commit

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Orlando Moraes Martins on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
